2573faf7c3c4b97c4ffe2776286d5c28eed4249d
shunsuke-tamura <wwwxx4869@gmail.com>
2023-06-12 17:27:13 +0900

    update: 一旦決まったデータを返すようにサーバ起動きた

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,10 @@
 module connect-back
 
 go 1.20
+
+require (
+	github.com/bufbuild/connect-go v1.8.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	taskv1 "connect-back/gen/rpc/task/v1"
+	"connect-back/gen/rpc/task/v1/taskv1connect"
+
+	"github.com/bufbuild/connect-go"
+	connect_go "github.com/bufbuild/connect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type TaskListServer struct{}
+
+func (s *TaskListServer) GetTaskList(ctx context.Context, req *connect_go.Request[taskv1.GetTaskListRequest]) (*connect_go.Response[taskv1.GetTaskListResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	testTasks := []*taskv1.Task{
+		{
+			Id:          "1",
+			UserId:      "123",
+			Name:        "GetTaskList",
+			IdCompleted: true,
+		},
+	}
+	res := connect.NewResponse(&taskv1.GetTaskListResponse{
+		Tasks: testTasks,
+	})
+	return res, nil
+}
+
+func (s *TaskListServer) CreateTask(ctx context.Context, req *connect_go.Request[taskv1.CreateTaskRequest]) (*connect_go.Response[taskv1.CreateTaskResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&taskv1.CreateTaskResponse{
+		CreatedId: "CreateTask",
+	})
+	return res, nil
+}
+
+func (s *TaskListServer) CompleteTask(ctx context.Context, req *connect_go.Request[taskv1.CompleteTaskRequest]) (*connect_go.Response[taskv1.CompleteTaskResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&taskv1.CompleteTaskResponse{})
+	return res, nil
+}
+
+func (s *TaskListServer) DeleteTask(ctx context.Context, req *connect_go.Request[taskv1.DeleteTaskRequest]) (*connect_go.Response[taskv1.DeleteTaskResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&taskv1.DeleteTaskResponse{})
+	return res, nil
+}
+
+func main() {
+	s := &TaskListServer{}
+	mux := http.NewServeMux()
+	path, handler := taskv1connect.NewTaskServiceHandler(s)
+	mux.Handle(path, handler)
+	log.Println("lounch server")
+	http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
